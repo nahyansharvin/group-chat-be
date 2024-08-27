f7b192e9224f3cdec143017e5b38347161a8b075
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-27 21:08:13 +0530

    feat: Add signup functionality to AuthController

---
+++ controllers/AuthController.js
@@ -0,0 +1,16 @@
+import User from "../models/UserModel";
+
+export const signup = async (req, res, next) => {
+    try {
+        const { firstName, lastName, email, password } = req.body;
+        if (!firstName || !lastName || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        await User.create({ firstName, lastName, email, password });
+        res.status(201).json({ message: "User created successfully", user: firstName + " " + lastName });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: error.message });
+    }
+}
