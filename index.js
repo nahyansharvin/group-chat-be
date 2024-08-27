f937f8cfdcb10f3ada721265450a0956961b6836
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-27 21:53:50 +0530

    chore: Add CORS and cookie-parser middleware

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
+import authRoutes from "./routes/AuthRoutes.js";
 
 dotenv.config();
 
@@ -9,7 +12,17 @@ const PORT = process.env.PORT || 3001;
 const databaseURL = process.env.DATABASE_URL;
 
 // Middleware
+app.use(cors({
+    origin: [process.env.ORIGIN],
+    credentials: true
+}));
+app.use(cookieParser());
 app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => res.send("Welcome to the Chat App API"));
+app.use("/api/auth", authRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
@@ -17,4 +30,4 @@ app.listen(PORT, () => {
 
 mongoose.connect(databaseURL)
     .then(() => console.log("Database connected"))
-    .catch((error) => console.log("Databse error: " ,error.message));
+    .catch((error) => console.log("Databse error: ", error.message));
