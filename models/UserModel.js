380dd3146771a1e1650b20154f6e7e16fb1d7a9b
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-27 20:54:12 +0530

    feat: Add bcrypt and create UserModel

---
+++ models/UserModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { genSalt, hash } from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "First name is required"]
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last name is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    }
+});
+
+userSchema.pre("save", async function(next) {
+    const salt = await genSalt();
+    this.password = await hash(this.password, salt);
+    next();
+});
+
+const User = mongoose.model("Users", userSchema);
+
+export default User;
