cb225825385dbe78fa37d0e1aab68faf403aa2d5
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-27 22:05:22 +0530

    feat: Add signin and signout functionality to AuthController

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,14 @@
 import User from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
+import { compare } from "bcrypt";
 
-export const signup = async (req, res, next) => {
+const maxAge = 3 * 24 * 60 * 60 * 1000; // 3 days
+
+const createToken = (email, userId) => {
+    return jwt.sign({ email, userId }, process.env.JWT_SECRET, { expiresIn: maxAge });
+}
+
+export const signup = async (req, res) => {
     try {
         const { firstName, lastName, email, password } = req.body;
         if (!firstName || !lastName || !email || !password) {
@@ -13,4 +21,42 @@ export const signup = async (req, res, next) => {
         console.log(error);
         return res.status(500).json({ message: error.message });
     }
-}
+};
+
+export const signin = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) return res.status(400).json({ message: "Email and password is required" });
+
+        const user = await User.findOne({ email });
+        if (!user) return res.status(404).json({ message: "User not found" });
+
+        const isMatch = await compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+        res.cookie("token", createToken(email, user._id), { 
+            httpOnly: true,
+            maxAge,
+            secure: true,
+            sameSite: "None"
+        });
+        return res.status(200).json({ 
+            message: "User signed in successfully",
+            user: {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+                userId: user._id
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: error.message });
+    }
+
+};
+
+export const signout = (req, res) => {
+    res.clearCookie("token");
+    res.status(200).json({ message: "User signed out successfully" });
+};
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { signup } from "../controllers/AuthController.js";
+import { signup, signin, signout } from "../controllers/AuthController.js";
 
 const authRoutes = Router();
 
 authRoutes.post("/signup", signup)
+authRoutes.post("/signin", signin)
+authRoutes.post("/signout", signout)
+
 
 export default authRoutes;
