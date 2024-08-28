2f2319bcf2b5b9b55402411ddffe75d685d48e7d
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-28 11:29:28 +0530

    feat: Add AuthMiddleware for token verification

---
+++ middlewares/AuthMiddleware.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(403).json({ message: "Invalid token" });
+
+        req.userId = decoded.userId;
+        next();
+    });
+};
