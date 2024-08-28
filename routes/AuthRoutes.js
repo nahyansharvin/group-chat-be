8e5dab12388c76ad3950943892e59961b474e267
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-28 13:13:08 +0530

    feat: Update AuthRoutes to include getAllUsers and updateUser endpoints

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,14 +1,21 @@
 import { Router } from "express";
-import { signup, signin, signout, getUser } from "../controllers/AuthController.js";
-import { verifyToken } from "../middlewares/AuthMiddleware.js";
+import { signup, signin, signout } from "../controllers/AuthController.js";
+import { isAdmin, verifyToken } from "../middlewares/AuthMiddleware.js";
+import { getAllUsers, getUser, updateUser } from "../controllers/ProfileController.js";
 
 const authRoutes = Router();
 
-authRoutes.post("/signup", signup)
 authRoutes.post("/signin", signin)
 authRoutes.post("/signout", signout)
+
 authRoutes.use(verifyToken)
 authRoutes.get("/user-info", getUser)
+authRoutes.get("/all-users", getAllUsers)
+
+// Admin routes
+authRoutes.use(isAdmin)
+authRoutes.post("/create-user", signup)
+authRoutes.patch("/update-user/:id", updateUser)
 
 
 export default authRoutes;
