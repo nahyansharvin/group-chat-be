02ce44485015c554b42f7d86a27d3b1258bd3c87
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-28 22:22:45 +0530

    feat: Add admin user creation in beforeAll hook

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -1,32 +1,52 @@
 import request from "supertest";
 import app from "../app.js";
 import mongoose from "mongoose";
+import User from "../models/UserModel.js";
+
+let cookie;
 
 beforeAll(async () => {
     await mongoose.connect(process.env.TEST_DATABASE_URL)
         .then(() => console.log("Test database connected"))
         .catch((error) => console.log("Databse error: ", error.message));
+    
+    // Create Admin User
+    await User.create({
+        firstName: "Test",
+        lastName: "Admin",
+        email: "admin1@gmail.com",
+        password: "Password@123",
+        role: "admin"
+    });
 });
 
 afterAll(async () => {
+    await mongoose.connection.db.dropDatabase();
     await mongoose.connection.close();
 });
 
 describe("Auth routes", () => {
-    it("should sign in a user", async () => {
-        const response = await request(app).post("/api/auth/signin")
-        .send({ 
-            email: "nahyan@chat.com",
-            password: "Password@123"
-        })
-        expect(response.status).toBe(200)
-        expect(response.body).toHaveProperty("user")
-
-    });
-
     it("Should return 401 if not signed in", async () => {
         const response = await request(app).get("/api/auth/user-info")
         expect(response.status).toBe(401)
     });
 
+    it("Sign in as admin", async () => {
+        const response = await request(app).post("/api/auth/signin")
+        .send({ 
+            email: "admin1@gmail.com",
+            password: "Password@123"
+        })
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveProperty("user")
+        cookie = response.headers["set-cookie"];
+    });
+
+    it("Should return user info", async () => {
+        const response = await request(app).get("/api/auth/user-info")
+        .set('cookie', cookie)
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveProperty("user")
+    });
+    
 });
