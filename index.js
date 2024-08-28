641d1652a3b460b7bfbc54cb027ca3ca511ddd04
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-29 00:39:43 +0530

    feat: Add socket.io integration for real-time communication

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 import app from "./app.js";
+import setupSocket from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 const databaseURL = process.env.DATABASE_URL;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+setupSocket(server);
 
 mongoose.connect(databaseURL)
     .then(() => console.log("Database connected"))
---
+++ socket/socket.js
@@ -0,0 +1,35 @@
+import { Server as SocketIOServer } from "socket.io";
+
+
+const setupSocket = (server) => {
+    const io =new SocketIOServer(server, {
+        cors: {
+            origin: [process.env.ORIGIN],
+            credentials: true
+        }
+    });
+
+    const userSocktets = new Map();
+
+    io.on('connection', (socket) => {
+        console.log(`Socket connection established with id ${socket.id}`);
+        const userId = socket.handshake.query.userId;
+
+        if(userId) {
+            userSocktets.set(userId, socket.id);
+            console.log(`User ${userId} connected with socket ${socket.id}`);
+        }
+
+        socket.on('disconnect', () => {
+            console.log('Socket connection disconnected');
+            userSocktets.forEach((value, key) => {
+                if(value === socket.id) {
+                    userSocktets.delete(key);
+                    console.log(`User ${key} disconnected`);
+                }
+            });
+        });
+    });
+};
+
+export default setupSocket;
