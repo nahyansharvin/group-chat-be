3e436fe15437fcdd939956ced3c61d1d3d285764
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-28 20:01:21 +0530

    chore: Refactor app.js and index.js for better code organization and testing

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/AuthRoutes.js";
+import profileRoutes from "./routes/ProfileRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors({
+    origin: [process.env.ORIGIN],
+    credentials: true
+}));
+app.use(cookieParser());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => res.send("Welcome to the Chat App API"));
+app.use("/api/auth", authRoutes);
+app.use("/api/users", profileRoutes);
+
+export default app;
--- index.js
+++ index.js
@@ -1,30 +1,8 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
-import authRoutes from "./routes/AuthRoutes.js";
-import profileRoutes from "./routes/ProfileRoutes.js";
+import app from "./app.js";
 
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const databaseURL = process.env.DATABASE_URL;
-
-// Middleware
-app.use(cors({
-    origin: [process.env.ORIGIN],
-    credentials: true
-}));
-app.use(cookieParser());
-app.use(express.json());
-
-// Routes
-app.get("/", (req, res) => res.send("Welcome to the Chat App API"));
-app.use("/api/auth", authRoutes);
-app.use("/api/users", profileRoutes);
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
