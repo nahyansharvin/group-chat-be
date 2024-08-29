cb25a238588da309fc1fd1a2bb9aa93d9b87bf3b
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-29 13:53:14 +0530

    feat: Add markAsRead function to MessageController

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -17,3 +17,22 @@ export const getMessages = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+export const markAsRead = async (req, res) => {
+    try {
+        const user1 = req.userId;
+        const { userId: user2 } = req.body;
+
+        const messages = await Message.updateMany(
+            { sender: user2, receiver: user1, read: false },
+            { read: true }
+        );
+
+        res.status(200).json({
+            message: `${messages.modifiedCount} Messages marked as read`,
+            data: messages
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
--- routes/MessageRoutes.js
+++ routes/MessageRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
-import { getMessages } from "../controllers/MessageController.js";
+import { getMessages, markAsRead } from "../controllers/MessageController.js";
 
 const messageRoutes = Router();
 
 messageRoutes.use(verifyToken)
 messageRoutes.get("/get-messages/:id", getMessages)
+messageRoutes.patch("/mark-as-read", markAsRead)
 
 export default messageRoutes;
