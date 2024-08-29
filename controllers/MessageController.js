716eab13288e63800d2510cb328a0e6f4eac0364
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-29 14:06:02 +0530

    feat: Add edit message route

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -12,7 +12,7 @@ export const getMessages = async (req, res) => {
             ]
         }).sort({ createdAt: 1 });
 
-        res.status(200).json(messages);
+        res.status(200).json({ messages });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -35,4 +35,25 @@ export const markAsRead = async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
-}
+};
+
+export const editMessage = async (req, res) => {
+    try {
+        const userId = req.userId;
+        const { messageId, message } = req.body;
+
+        const existingMessage = await Message.findById(messageId);
+
+        if (!existingMessage) return res.status(404).json({ message: "Message with given Id not found" });
+        if (existingMessage.sender.toString() !== userId) return res.status(403).json({ message: "You are not allowed to edit others' message" });
+
+        const updatedMessage = await Message.findByIdAndUpdate(messageId, { message }, { new: true });
+
+        res.status(200).json({
+            message: "Message updated successfully",
+            data: updatedMessage
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
--- routes/MessageRoutes.js
+++ routes/MessageRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
-import { getMessages, markAsRead } from "../controllers/MessageController.js";
+import { editMessage, getMessages, markAsRead } from "../controllers/MessageController.js";
 
 const messageRoutes = Router();
 
 messageRoutes.use(verifyToken)
 messageRoutes.get("/get-messages/:userId", getMessages)
 messageRoutes.patch("/mark-as-read", markAsRead)
+messageRoutes.patch("/edit-message", editMessage)
 
 export default messageRoutes;
