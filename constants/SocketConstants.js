031bf6de9ced35913a72aa16b6fd5a3501914e4f
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-30 22:54:59 +0530

    refactor: Update SocketConstants to include new socket event for deleting messages

--- constants/SocketConstants.js
+++ constants/SocketConstants.js
@@ -1,7 +1,8 @@
 export const SOCKET_EVENTS = {
   DIRECT_MESSAGE: "direct-message",
-  DELETE_DIRECT_MESSAGE: "delete-direct-message",
+  
   EDIT_MESSAGE: "edit-message",
+  DELETE_MESSAGE: "delete-message",
   MARK_AS_READ: "mark-as-read",
   
   GROUP_MESSAGE: "group-message",
