219d70ee6fd2929b21e88d9bc066a2e66a80ff04
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-30 22:58:07 +0530

    feat: Add validateMessageSender utility function

---
+++ utils/validateMessageSender.js
@@ -0,0 +1,8 @@
+import Message from "../models/Message.js";
+
+export const validateMessageSender = async (messageId, senderId) => {
+    const message = await Message.findById(messageId);
+    if (!message) throw new Error("Message with given Id not found");
+    if (message.sender.toString() !== senderId) throw new Error("You are not allowed to edit others' message");
+    return message;
+}
