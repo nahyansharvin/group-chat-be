b4ddc7a945a5fe924e1b373aec2a571350b8f389
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-30 16:02:20 +0530

    refactor: Add new socket event constants for direct messages and error handling

--- constants/SocketConstants.js
+++ constants/SocketConstants.js
@@ -1,4 +1,10 @@
 export const SOCKET_EVENTS = {
-  MESSAGE: "message",
+  DIRECT_MESSAGE: "direct-message",
+  DELETE_DIRECT_MESSAGE: "delete-direct-message",
+  EDIT_MESSAGE: "edit-message",
+  MARK_AS_READ: "mark-as-read",
+  
   GROUP_MESSAGE: "group-message",
+  
+  ERROR: "error",
 };
