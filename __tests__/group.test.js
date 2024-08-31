14fae0ce64f93bdd63421f52162f9aea683d7657
Nahyan Sharvin <nskonnalath@gmail.com>
2024-08-31 16:01:56 +0530

    feat: Add test for leave group endpoint

--- __tests__/group.test.js
+++ __tests__/group.test.js
@@ -3,10 +3,11 @@ import app from "../app.js";
 
 const userCookie = global.userCookie;
 const userToAdd = global.adminId;
+const cookieToLeave = global.adminCookie;
 let groupId;
 
 describe("Group routes", () => {
-    it("Create a new group", async () => {
+    it("Should create a new group", async () => {
         const response = await request(app).post("/api/groups/create-group")
         .set('cookie', userCookie)
         .send({
@@ -18,7 +19,7 @@ describe("Group routes", () => {
         groupId = response.body.data.groupId;
     });
 
-    it("Edit group", async () => {
+    it("Should edit group", async () => {
         const response = await request(app).patch(`/api/groups/edit-group/${groupId}`)
         .set('cookie', userCookie)
         .send({
@@ -29,7 +30,7 @@ describe("Group routes", () => {
         expect(response.body.data).toHaveProperty("name", "Edited Group")
     });
 
-    it("Get all groups", async () => {
+    it("Should get all groups", async () => {
         const response = await request(app).get("/api/groups/get-groups")
         .set('cookie', userCookie)
         expect(response.status).toBe(200)
@@ -37,7 +38,7 @@ describe("Group routes", () => {
         expect(response.body.groups).toBeInstanceOf(Array)
     });
 
-    it("Search groups", async () => {
+    it("Should search groups", async () => {
         const response = await request(app).get("/api/groups/search?filter=edi")
         .set('cookie', userCookie)
         expect(response.status).toBe(200)
@@ -45,7 +46,7 @@ describe("Group routes", () => {
         expect(response.body.groups).toBeInstanceOf(Array)
     });
 
-    it("Add members to group", async () => {
+    it("Should add members to group", async () => {
         const response = await request(app).patch(`/api/groups/add-members/${groupId}`)
         .set('cookie', userCookie)
         .send({
@@ -57,7 +58,7 @@ describe("Group routes", () => {
         expect(response.body.data.members).toBeInstanceOf(Array)
     });
 
-    it("Remove members from group", async () => {
+    it("Should remove members from group", async () => {
         const response = await request(app).patch(`/api/groups/remove-members/${groupId}`)
         .set('cookie', userCookie)
         .send({
@@ -69,7 +70,21 @@ describe("Group routes", () => {
         expect(response.body.data.members).toBeInstanceOf(Array)
     });
 
-    it("Delete group", async () => {
+    it("Should leave the group", async () => {
+        const response1 = await request(app).patch(`/api/groups/add-members/${groupId}`)
+        .set('cookie', userCookie)
+        .send({
+            members: [userToAdd]
+        })
+        expect(response1.status).toBe(200)
+        const response2 = await request(app).patch(`/api/groups/leave-group/${groupId}`)
+        .set('cookie', cookieToLeave)
+        expect(response2.status).toBe(200)
+        expect(response2.body).toHaveProperty("message", "You have left the group successfully")
+        expect(response2.body.groupId).toBe(undefined)
+    });
+
+    it("Should delete group", async () => {
         const response = await request(app).delete(`/api/groups/delete-group/${groupId}`)
         .set('cookie', userCookie)
         expect(response.status).toBe(200)
